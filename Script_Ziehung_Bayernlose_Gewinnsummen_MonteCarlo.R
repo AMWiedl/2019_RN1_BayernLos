66f37418b584258e8c3da97f64cf110b82af2739
AMWiedl <alexander.wiedl@outlook.de>
2019-01-11 23:17:49 +0100

    Added Pool & Probabilities

--- Script_Ziehung_Bayernlose_Gewinnsummen_MonteCarlo.R
+++ Script_Ziehung_Bayernlose_Gewinnsummen_MonteCarlo.R
@@ -1,12 +1,14 @@
-#Lospool definieren mit 0
-
+#Lospool definieren inkl. 0
+LosPool<-c(0,2,4,10,20,100,1000,10000,300000)
 
 #Wahrscheinlichkeiten für jedes Los definieren
+ProbLose_ohne0<-c((1/5.88),(1/20),(1/50),(1/200),(1/1000),(1/100000),(1/1666666.67),(1/5000000))
+GesamtSofortGewProb<-(sum(ProbLose))
+ProbLose_mit0<-c((1-GesamtSofortGewProb),(1/5.88),(1/20),(1/50),(1/200),(1/1000),(1/100000),(1/1666666.67),(1/5000000))
 
 #Anzahl Lose je Ziehung definieren
-
+Lose<-1000
 #Anzahl Ziehungen definieren
-
+Ziehungen<-100000
 #Ziehungen durchführen und Gewinnsumme je Ziehung ermitteln
 
-Ziehungen<-replicate(100000,{X<-sample(LosPool,size=1000,replace=TRUE,prob=ProbLose)})
